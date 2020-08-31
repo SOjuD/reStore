19ffafb386eb1c48c1ee02362df2f2d0f3ec334c
Oleg <44555176+SOjuD@users.noreply.github.com>
2020-08-31 22:31:19 +0300

    add async bookstore-service

---
+++ src/components/book-list/book-list.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import BookListItem from '../book-list-item';
+import { connect } from 'react-redux';
+
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
+import Spinner from '../spinner';
+
+import './book-list.css';
+
+class BookList extends Component {
+
+  componentDidMount() {
+    // 1. receive data
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks()
+      .then((data)=> {
+        // 2. dispacth action to store
+        booksLoaded(data);
+      });
+
+  }
+
+  render() {
+    const { books, loading } = this.props;
+    if(loading) {
+      return (
+        <Spinner />
+      );
+    }
+    return (
+      <ul className="book-list">
+        {
+          books.map((book) => {
+            return (
+              <li key={book.id}><BookListItem book={book}/></li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
+};
+
+const mapDispatchToProps = {
+  booksLoaded
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
